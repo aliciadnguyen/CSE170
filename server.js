f2a6b4bb05609e5869bd53219e2c0e6eaf29ea49
Giovanni Wong <deargiovanni89@gmail.com>
2015-02-05 20:44:10 -0800

    routing now works

--- server.js
+++ server.js
@@ -24,9 +24,9 @@
 
 
        // application -------------------------------------------------------------
-    /*app.get('*', function(req, res) {
+    app.get('*', function(req, res) {
         res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end
-    });*/
+    });
 
 
     var port = process.env.PORT || PORT; // 80 for web, 3000 for development
