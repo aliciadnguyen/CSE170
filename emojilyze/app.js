af3bf3ba1c3d9d67132c197e2a8a13bcc6f94a3b
Giovanni Wong <deargiovanni89@gmail.com>
2015-02-08 19:59:53 -0800

    heroku app not working, trying to fix, commenting out mongoose in app.js

--- emojilyze/app.js
+++ emojilyze/app.js
@@ -4,10 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
+//var mongoose = require('mongoose');
 
 
-mongoose.connect('mongodb://localhost/emojilyze');
+//mongoose.connect('mongodb://localhost/emojilyze');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
