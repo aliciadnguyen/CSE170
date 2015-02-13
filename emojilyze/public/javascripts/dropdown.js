191f70d87ddf95a154ed16d160e04f5b14361e2e
Anh Tran <aptran@ucsd.edu>
2015-02-12 23:36:11 -0800

    worked on sorting page to loop through emojis from the json file

--- emojilyze/public/javascripts/dropdown.js
+++ emojilyze/public/javascripts/dropdown.js
@@ -32,5 +32,46 @@ angular.module('myApp.controllers').controller('privacySwitch',['$scope',functio
 
 
 
+angular.module('myApp.controllers').controller('SortingCtrl', ['$scope',function($scope){
+
+  var data = $.get('emoji.json');
+  var index = 0;
+
+  $(".dropdown-menu").on('click', 'li a', function(){
+    $(".categoryBtn:first-child").html($(this).text()+" <span class=\"caret\"></span>");
+    $(".categoryBtn:first-child").val($(this).text());
+
+  });
+
+  $scope.submitCategory = function() {
+    console.log('Submit clicked');
+    console.log($('#customCategory').val());
+
+    //get emoji information
+    var emojiArray = data['responseJSON']['emoji'];
+    var currEmoji = emojiArray[index];
+
+    //update index
+    if(index == emojiArray.length-1) {
+      index = 0;
+    }
+    else {
+      index++;
+    }
+    var newEmoji = emojiArray[index];
+    var newImage = newEmoji['imageSRC'];
+
+    //set image to next emoji
+    $('#emojiImage').attr( {src: newImage} );
+
+    //clear input field
+    $('#customCategory').val("");
+
+  }
+
+}]);
+
+
+
 }());
 
