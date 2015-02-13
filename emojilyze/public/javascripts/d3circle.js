c523cbaab9e63d15b1870afd2f4d2d1b628fdef4
Giovanni Wong <deargiovanni89@gmail.com>
2015-02-12 22:47:06 -0800

    finsished getting data from json using angular

--- emojilyze/public/javascripts/d3circle.js
+++ emojilyze/public/javascripts/d3circle.js
@@ -7,9 +7,11 @@
         restrict: 'EA',
         link: function(scope, iElement, iAttrs) {
           
-         d3.json("data.json", function(error, data) {
-            data.forEach(function(d) {
-                d.texts = +d.texts;
+         d3.json("data.json", function(data) {
+              
+
+              data.forEach(function(d) {
+                d.timesUsed += d.timesUsed;
         });  
           
         var width = (window.innerWidth < 1280) ? 400 : 600,
@@ -31,7 +33,7 @@
             .sort(null)
             .startAngle(1.1*Math.PI)
             .endAngle(3.1*Math.PI)
-            .value(function(d) { return d.texts; });
+            .value(function(d) { return d.timesUsed; });
 
          var svg = d3.select("#svgPie")
             .append("svg")
@@ -53,11 +55,11 @@
             .append("g")
             .attr("d", "arc")
             .attr('fill', function(d, i) { 
-              return color(d.data.mood); 
+              return color(d.data.defaultCategory); 
             });
 
           g.append("path")
-              .style("fill", function(d) { return color(d.data.mood); })
+              .style("fill", function(d) { return color(d.data.defaultCategory); })
               .transition().delay(function(d, i) { return i * 500; }).duration(500)
                 .attrTween('d', function(d) {
                   var i = d3.interpolate(d.startAngle+0.1, d.endAngle);
@@ -70,10 +72,10 @@
          
           g.on("mouseover", function(d) {
               var total = d3.sum(data.map(function(d) {                // NEW
-              return d.texts;                                           // NEW
+              return d.timesUsed;                                           // NEW
             })); 
 
-              var percent = Math.round(1000 * d.data.texts / total) / 10;
+              var percent = Math.round(1000 * d.data.timesUsed / total) / 10;
               text1 = g.append("text")
                   .attr("translate", arc.centroid(d))
                   .attr("dy", ".5em")
@@ -81,7 +83,7 @@
                   .style("text-anchor", "middle")
                   .style("fill", "black")
                   .attr("class", "on")
-                  .text(d.data.mood);
+                  .text(d.data.defaultCategory);
 
                text2 = g.append("text")
                   .attr("translate", arc.centroid(d))
@@ -104,7 +106,7 @@
               .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
               .attr("dy", ".35em")
               .style("text-anchor", "middle")
-              .text(function(d) { return d.data.mood; });*/
+              .text(function(d) { return d.data.defaultCategory; });*/
 
         });
 
