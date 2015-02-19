c31fdac951ec46f7380ec5e109d8a4126a53c57e
Anh Tran <aptran@ucsd.edu>
2015-02-19 01:33:49 -0800

    updated sorting page. grabbing emoji categories from json

---
+++ public/javascripts/sorting.js
@@ -0,0 +1,74 @@
+(function () {
+  'use strict';
+
+
+angular.module('myApp.controllers').controller('SortingCtrl', ['$scope', '$http', 'emojiService', function($scope, $http, emojiService){
+  $scope.emojiArray = null;
+
+  emojiService.getData().then(function(response) {
+      $scope.emojiArray = response.data.emoji;
+
+      var index = 0;
+      var categoryArray = [];
+
+      for(var i = 0; i < $scope.emojiArray.length; i++) {
+        var currEmoji = $scope.emojiArray[i];
+        var category = currEmoji['customCategory'];
+
+        categoryArray[i] = category;
+      }
+
+      $scope.list = categoryArray;  
+      $scope.current = categoryArray[0];
+  });
+
+
+  $(".dropdown-menu").on('click', 'li a', function(){
+    $(".categoryBtn:first-child").html($(this).text()+" <span class=\"caret\"></span>");
+    $(".categoryBtn:first-child").val($(this).text());
+
+  });
+
+  var index = 0;
+
+  $scope.submitCategory = function() {
+    console.log('Submit clicked');
+    console.log($('#customCategory').val());
+
+    //update current emoji category - FIX THISSSSS
+    $scope.emojiArray[index]['customCategory'] = $('#customCategory').val();
+
+    //update index
+    if(index == $scope.emojiArray.length-1) {
+      index = 0;
+    }
+    else {
+      index++;
+    }
+
+    var newEmoji = $scope.emojiArray[index];
+    var newImage = newEmoji['imageSRC'];
+
+    //set image to next emoji
+    $('#emojiImage').attr( {src: newImage} );
+
+    //clear input field
+    $('#customCategory').val("");
+
+    $(".categoryBtn:first-child").html(newEmoji['customCategory']+" <span class=\"caret\"></span>");
+    $(".categoryBtn:first-child").val(newEmoji['customCategory']);
+
+  }
+
+}])
+
+
+.factory('emojiService', function($http) {
+    return {
+      getData : function() {
+        return $http.get('emoji.json');
+      }
+    };
+});
+
+}());
