f41a41429ba691ecee691c06716617dcd11863d6
unknown <aliciadnguyen@gmail.com>
2015-02-28 21:22:31 -0800

    added buttons and more to mood alt page

--- public/javascripts/mymoodalt.js
+++ public/javascripts/mymoodalt.js
@@ -1,4 +1,20 @@
-(function(){
+(function () {
+  'use strict';
+
+  angular.module('myApp.controllers').controller('mymoodaltCtrl',['$scope','$http',function($scope,$http){
+    $http.get('friend.json')
+      .then(function(res){
+        $scope.friends = res.data;
+        console.log($scope.friends);
+      });
+      $scope.myVar = false;
+
+      // Pop up button
+      $(function () {
+        $('[data-toggle="popover"]').popover()
+      })
+
+      // Easy pie charts
       var $ppc = $('.progress-pie-chart'),
         percent = parseInt($ppc.data('percent')),
         deg = 360*percent/100;
@@ -7,5 +23,6 @@
       }
       $('.ppc-progress-fill').css('transform','rotate('+ deg +'deg)');
       $('.ppc-percents span').html(percent+'%');
-});
-   
+      
+  }]);
+}());
